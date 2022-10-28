983b443bcbe5ebc3e43b144f9ac0fc851eb0cbd8
Davide Carassiti <dcarassi@e3r6p2.42roma.it>
2022-10-28 23:14:59 +0200

    commit strani

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,28 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 22:18:08 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/28 23:04:25 by dcarassi         ###   ########.fr       */
+/*   Created: 2022/10/28 22:32:59 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/28 23:14:33 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-	int	multipl;
+	int multipl;
 
 	multipl = 1;
-	while (nb > 0)
+	if (nb > 0)
 	{
-		multipl *= nb;
-		nb--;
+		multipl *= nb * ft_recursive_factorial(nb - 1);
+		nb-=1;
 	}
 	if (nb < 0)
 		return (0);
 	return (multipl);
+}
+
+int	main (void)
+{
+	printf("%d", ft_recursive_factorial(4));
+	return (0);
 }
