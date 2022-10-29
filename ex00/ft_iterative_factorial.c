5d5f85cb1902c885949a6f4871e14b7370d0972c
Davide Carassiti <dcarassi@e3r4p2.42roma.it>
2022-10-29 16:25:11 +0200

    commit di sabato pomeringhio

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 22:18:08 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/28 23:04:25 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/29 16:17:24 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int	ft_iterative_factorial(int nb)
 	int	multipl;
 
 	multipl = 1;
+	if (nb == 0)
+		return (multipl);
+	else if (nb < 0)
+		return (0);
 	while (nb > 0)
 	{
 		multipl *= nb;
 		nb--;
 	}
-	if (nb < 0)
-		return (0);
 	return (multipl);
 }
--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 22:32:59 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/29 11:27:53 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/29 16:20:01 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ int	ft_recursive_factorial(int nb)
 	int	multipl;
 
 	multipl = 1;
+	if (nb < 0)
+		return (0);
+	else if (nb == 0 || nb == 1)
+		return (1);
 	if (nb > 0)
 	{
 		multipl *= nb * ft_recursive_factorial(nb - 1);
 		nb -= 1;
 	}
-	if (nb < 0)
-		return (0);
 	return (multipl);
 }
