7ad514c86e636142754dcadca7e21b3264554e1d
Davide Carassiti <dcarassi@e3r8p5.42roma.it>
2022-10-31 09:50:33 +0100

    last c05 hopefully

--- ex06/ft_is_prime.c
+++ ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 18:41:09 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/31 09:19:32 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/31 09:46:51 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,19 @@
 
 int	ft_is_prime(int nb)
 {
-	int	primesheck;
 	int	div;
 
 	div = 2;
-	primesheck = 0;
-	if (nb == 2)
-		return (1);
+	if (nb == 0 || nb == 1)
+		return (0);
 	else if (nb > 2)
 	{
-		while (div <= (nb / 2) && primesheck == 0)
+		while (div < nb)
 		{
 			if (nb % div == 0)
-				primesheck = primesheck + 1;
+				return (0);
 			div = div + 1;
 		}
 	}
-	if (primesheck == 0)
-		return (1);
-	else
-		return (0);
+	return (1);
 }
